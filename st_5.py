823dd0cc08ac734c49710aa6368a64dbf3e32ad0
Star <zhfeng012@gmail.com>
2025-03-14 16:45:20 +0800

    20250314

--- st_5.py
+++ st_5.py
@@ -107,8 +107,8 @@ def calculate_trade_stats(trading_curve):
     sharpe = returns.mean() / returns.std() if returns.std() > 0 else 0
     return sharpe, len(returns), win_rate
 file = st.selectbox('选择对应的文件:',os.listdir("pickle/"), key='global_window')
-LOSS_THRESHOLD= st.number_input("下跌阈值-- 1/10000", 0, 100, 1)*-1
-PROFIT_TARGET = st.number_input("上涨比率-- 1/10000", 0, 100, 1)
+LOSS_THRESHOLD= st.number_input("下跌阈值-- 1/10000", 0, 100, 1)/-10000
+PROFIT_TARGET = st.number_input("上涨比率-- 1/10000", 0, 100, 1)/10000
 MAX_HOLDING = st.number_input("最多持仓天数-- 1/10000", 0, 100, 1)
 daily_returns = pickle.load(open('pickle/'+file, 'rb'))
 if daily_returns is not None:
